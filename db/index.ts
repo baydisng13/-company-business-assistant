814b0db69337828b0342b71b2c396ef3ed34c478
Theodore <baydisng13@gmail.com>
2025-07-06 18:44:04 +0300

    refactor: update database connection handling and switch to postgres-js

--- db/index.ts
+++ db/index.ts
@@ -1,5 +1,14 @@
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { drizzle } from 'drizzle-orm/postgres-js'
 import * as schema from './schema';
+import postgres from 'postgres'
 
 
-export const db = drizzle(process.env.POSTGRES_URL!, { schema })
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error('POSTGRES_URL is not set')
+}
+// Disable prefetch as it is not supported for "Transaction" pool mode
+export const client = postgres(connectionString, { prepare: false })
+
+export const db = drizzle(client, { schema });
